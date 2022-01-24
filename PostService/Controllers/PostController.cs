1809e8a7fa6435cfcc8e01c33d39f139ae985109
Willian Santos <wilian.santos@msn.com>
2022-01-24 08:42:41 -0300

    adicionando post service

---
+++ PostService/Controllers/PostController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using PostService.Data;
+using PostService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly PostServiceContext _context;
+        public PostController(PostServiceContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Post>>> GetPost()
+        {
+            return await _context.Posts.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IEnumerable<Post>>> PostUser(Post post)
+        {
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetPost", new { id = post.PostId }, post);
+        }
+    }
+}
---
+++ PostService/Data/PostServiceContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostService.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostService.Data
+{
+    public class PostServiceContext : DbContext
+    {
+        public PostServiceContext(DbContextOptions<PostServiceContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
